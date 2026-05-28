e9527be4e284f4f0045247c8aec22d4709315e98
cejidb <cejidb@mail.ru>
2026-05-28 11:12:56 +0300

    chore(tooling): обновить commitlint.config.cjs

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "docs", "chore", "refactor", "test", "ci", "build", "perf", "revert"]],
-    "header-max-length": [2, "always", 100]
-  }
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "chore", "refactor", "test", "ci", "build", "perf", "revert"],
+    ],
+    "header-max-length": [2, "always", 100],
+  },
 };
